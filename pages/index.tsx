af1954a85e986905f51c4ce1c1e84f57dd8b60de
jamesjf7 <jamesjfoong2000@gmail.com>
2022-12-12 15:01:42 +0700

    Revert "Merge branch 'wrong-branch' of github.com:jamesjf7/portfolio into wrong-branch"
    This reverts commit e3a094e72f5a4eea1eea64c26fa54d08ced347f1, reversing
    changes made to 8e101701e2b193522dd27d8ad9200ff5866ada59.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      New Branch
+      Hello World!
     </div>
   )
 }
--- pages/newBranch.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function newBranch() {
-  return (
-    <div>newBranch</div>
-  )
-}
-
-export default newBranch
